4535379cb8c31d64c52599f2f9fa5f559953766b
AngieCastano1634 <1634@holbertonschool.com>
2021-03-09 22:39:06 +0000

    Test Rick & m orty Api

---
+++ build/precache-manifest.f1610f555d72dd01f48f4d40660286b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e1edd4510c19b264079",
+    "url": "/static/css/main.cbbef17d.chunk.css"
+  },
+  {
+    "revision": "0e1edd4510c19b264079",
+    "url": "/static/js/main.0e1edd45.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5e9ca2183b308286ee82",
+    "url": "/static/css/2.12e2f7d5.chunk.css"
+  },
+  {
+    "revision": "5e9ca2183b308286ee82",
+    "url": "/static/js/2.5e9ca218.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "db3a9e3f7f984c276953272ba88a2f06",
+    "url": "/index.html"
+  }
+];
